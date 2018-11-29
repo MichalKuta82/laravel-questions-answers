102d80f0fea0f267fb5b9e5560c43e00ecaeb4c4
Michal Kuta <michal@webmedia.ie>
2018-11-29 22:17:23 +0000

    Vue.js initialized to manipulate the DOM

--- resources/views/weather-js.blade.php
+++ resources/views/weather-js.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container" id="app">
     <div class="row">
-        <div class="col-md-6 col-md-offset-3">
+        <div class="col-md-6 col-md-offset-3" v-if="step !== 1">
             <h1>Enter An Address To Get Weather</h1>
 
             <form>
               <div class="form-group">
                 <label for="location">Location</label>
-                <input type="text" class="form-control" name="location" placeholder="Enter Zipcode or Address">
+                <input type="text" class="form-control" name="location" placeholder="Enter Zipcode or Address" v-model="userInput">
               </div>
-              <button type="submit" name="submit" class="btn btn-primary">Create</button>
+              <button class="btn btn-primary" v-on:click.prevent="getWeather" v-show="userInput">Get Weather</button>
         </div>
-        <div class="col-md-6 col-md-offset-3">
+        <div class="col-md-6 col-md-offset-3" v-else>
             <h1>Address</h1>
 
             <hr>
@@ -30,6 +30,19 @@
 
 @section('scripts')
 <script>
-  
+
+  var app = new Vue({
+    el: '#app',
+    data: {
+      step: 1,
+      userInput: ''
+    },
+    methods: {
+      getWeather: function() {
+        this.step = 2
+      }
+    }
+  });
+
 </script>
 @endsection
